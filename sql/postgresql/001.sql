066f74bde8100369181cb20417f8939e86bcb401
Neville Dipale <nevilledips@gmail.com>
2020-04-05 12:41:03 +0200

    disable buggy SQL conversions

--- sql/postgresql/001.sql
+++ sql/postgresql/001.sql
@@ -8,17 +8,45 @@ create table arrow_data_types (
 	smallints smallint,
 	ints int,
 	bigints bigint,
-	bytes bytea,
-	doubles double precision,
-	intervals interval,
-	times time without time zone,
-	timestamps timestamp without time zone,
-	timestampstz timestamp with time zone,
-	uuids uuid
+-- 	bytes bytea,
+	doubles double precision
+-- 	, intervals interval,
+-- 	times time without time zone,
+-- 	timestamps timestamp without time zone,
+-- 	timestampstz timestamp with time zone,
+-- 	uuids uuid
 );
 
-insert into arrow_data_types (booleans, chars, strings, texts, dates, smallints, ints, bigints, bytes, doubles, intervals, times, timestamps, timestampstz, uuids)
+insert into arrow_data_types (
+	booleans, 
+	chars, 
+	strings, 
+	texts, 
+	dates, 
+	smallints, 
+	ints, 
+	bigints, 
+-- 	bytes, 
+	doubles
+-- 	, intervals, 
+-- 	times, 
+-- 	timestamps, 
+-- 	timestampstz, 
+-- 	uuids
+)
 values
-	(true, 'A', 'Lorem', 'Lorem', '2020-01-01', 23, 23333, 2333333333, E'\\xabcdef', -159.225, '1-10', '01:23:45', '2020-01-01 01:23:45.678', '2020-01-01 02:23:45.678+01', 'a704b2ee-0668-42bf-99b7-ea1f794edea9'),
-	(false, '3', 'Ipsum', 'Ipsum', '2020-01-02', -16, -23333, -2333333333, E'\\x00000000ab', 159.225, '2-5', '23:59:59', '2020-01-02 01:23:45.678', '2020-01-02 02:23:45.678+01', 'b704b2ee-0668-42bf-99b7-ea1f794edea9')
+	(
+		true, 'A', 'Lorem', 'Lorem', '2020-01-01', 23, 23333, 2333333333, 
+-- 		E'\\xabcdef', 
+		-159.225
+-- 		, '1-10', '01:23:45', '2020-01-01 01:23:45.678', '2020-01-01 02:23:45.678+01', 'a704b2ee-0668-42bf-99b7-ea1f794edea9'
+	),
+	(
+		false, '3', 'Ipsum', 'Ipsum', '2020-01-02', -16, -23333, -2333333333, 
+-- 		E'\\x00000000ab', 
+		159.225
+-- 		, '2-5', '23:59:59', '2020-01-02 01:23:45.678', '2020-01-02 02:23:45.678+01', 'b704b2ee-0668-42bf-99b7-ea1f794edea9'
+	)
 ;
+
+select column_name, ordinal_position, is_nullable, data_type, character_maximum_length, numeric_precision, datetime_precision from information_schema.columns where table_name = 'arrow_data_types'
