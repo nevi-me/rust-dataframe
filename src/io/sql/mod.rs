2b879731ea863411902567a0426518bfc81dfac5
Neville Dipale <nevilledips@gmail.com>
2020-04-06 12:00:22 +0200

    WIP SQL write support

--- src/io/sql/mod.rs
+++ src/io/sql/mod.rs
@@ -4,6 +4,7 @@ use arrow::datatypes::Schema;
 use arrow::record_batch::RecordBatch;
 
 use crate::error::Result;
+use std::sync::Arc;
 pub trait SqlDataSource {
     fn get_table_schema(connection: &str, table_name: &str) -> Result<Schema>;
     fn read_table(
@@ -18,4 +19,10 @@ pub trait SqlDataSource {
         limit: Option<usize>,
         batch_size: usize,
     ) -> Result<Vec<RecordBatch>>;
+}
+
+pub trait SqlDataSink {
+    fn create_table(connection: &str, table_name: &str, schema: &Arc<Schema>) -> Result<()>;
+    fn write_to_table(connection: &str, table_name: &str, batches: Vec<&RecordBatch>)
+        -> Result<()>;
 }
